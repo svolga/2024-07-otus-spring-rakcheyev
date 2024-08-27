f6545d99e05a269ee0b2e0e032c82b4147204402
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2024-08-27 23:33:46 +0300

    feat: Удалить использование профиля

--- hw03-spring-boot/src/main/java/ru/otus/hw/runner/AppRunner.java
+++ hw03-spring-boot/src/main/java/ru/otus/hw/runner/AppRunner.java
@@ -2,13 +2,11 @@ package ru.otus.hw.runner;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import ru.otus.hw.service.TestRunnerService;
 
 @Component
 @RequiredArgsConstructor
-@Profile("!test")
 public class AppRunner implements CommandLineRunner {
 
     private final TestRunnerService testRunnerService;
