a319ed0874a5f274c25f3fd81fade37bea7d2b14
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2024-08-27 11:09:59 +0300

    feat: Добавить класс тестирования со @SpringBootTest

--- hw03-spring-boot/src/test/java/ru/otus/hw/ApplicationTest.java
+++ hw03-spring-boot/src/test/java/ru/otus/hw/ApplicationTest.java
@@ -1,15 +1,11 @@
 package ru.otus.hw;
 
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
+@ActiveProfiles("application")
 @SpringBootTest
 public class ApplicationTest {
 
-    @Test
-    void contextLoads() {
-    }
 }
