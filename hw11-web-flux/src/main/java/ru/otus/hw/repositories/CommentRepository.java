201e45d54bf73ecf29fc4b33d1b011ead6a304f0
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2025-01-17 18:41:03 +0300

    style: Удалить unused import

--- hw11-web-flux/src/main/java/ru/otus/hw/repositories/CommentRepository.java
+++ hw11-web-flux/src/main/java/ru/otus/hw/repositories/CommentRepository.java
@@ -5,8 +5,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import ru.otus.hw.models.Comment;
 
-import java.util.List;
-
 public interface CommentRepository extends ReactiveMongoRepository<Comment, String> {
     Flux<Comment> findAllByBookId(String bookId);
     Mono<Void> deleteAllByBookId(String bookId);
--- hw11-web-flux/src/main/java/ru/otus/hw/repositories/GenreRepository.java
+++ hw11-web-flux/src/main/java/ru/otus/hw/repositories/GenreRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
 import ru.otus.hw.models.Genre;
 
-import java.util.List;
 import java.util.Set;
 
 public interface GenreRepository extends ReactiveMongoRepository<Genre, String> {
