479e1cd04f45c39220976f910d7cadff4e7ac94f
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2025-01-17 18:39:35 +0300

    fix: Заменить List->Flux, void -> Mono<Void>

--- hw11-web-flux/src/main/java/ru/otus/hw/repositories/CommentRepository.java
+++ hw11-web-flux/src/main/java/ru/otus/hw/repositories/CommentRepository.java
@@ -1,11 +1,13 @@
 package ru.otus.hw.repositories;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import ru.otus.hw.models.Comment;
 
 import java.util.List;
 
 public interface CommentRepository extends ReactiveMongoRepository<Comment, String> {
-    List<Comment> findAllByBookId(String bookId);
-    void deleteAllByBookId(String bookId);
+    Flux<Comment> findAllByBookId(String bookId);
+    Mono<Void> deleteAllByBookId(String bookId);
 }
