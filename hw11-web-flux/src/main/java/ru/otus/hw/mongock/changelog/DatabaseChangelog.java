6f417cfac8d52df51cc7b6a787456cf41bc53fda
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2025-01-22 17:45:25 +0300

    feat: Обновить changelog

--- hw11-web-flux/src/main/java/ru/otus/hw/mongock/changelog/DatabaseChangelog.java
+++ hw11-web-flux/src/main/java/ru/otus/hw/mongock/changelog/DatabaseChangelog.java
@@ -2,13 +2,16 @@ package ru.otus.hw.mongock.changelog;
 
 import com.github.cloudyrock.mongock.ChangeLog;
 import com.github.cloudyrock.mongock.ChangeSet;
-import com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.decorator.impl.MongockTemplate;
 import com.mongodb.client.MongoDatabase;
 import lombok.val;
 import ru.otus.hw.models.Author;
 import ru.otus.hw.models.Book;
 import ru.otus.hw.models.Genre;
 import ru.otus.hw.models.Comment;
+import ru.otus.hw.repositories.AuthorRepository;
+import ru.otus.hw.repositories.BookRepository;
+import ru.otus.hw.repositories.CommentRepository;
+import ru.otus.hw.repositories.GenreRepository;
 
 @ChangeLog
 public class DatabaseChangelog {
@@ -36,46 +39,46 @@ public class DatabaseChangelog {
     private Book book1;
 
     private Book book2;
-
     @ChangeSet(order = "001", id = "dropDb", author = AUTHOR_NAME, runAlways = true)
     public void dropDb(MongoDatabase db) {
         db.drop();
     }
 
     @ChangeSet(order = "002", id = "insertAuthors", author = AUTHOR_NAME, runAlways = true)
-    public void insertAuthors(MongockTemplate repository) {
-        author1 = repository.save(new Author("1", "Author_1"));
-        author2 = repository.save(new Author("2", "Author_2"));
-        author3 = repository.save(new Author("3", "Author_3"));
+    public void insertAuthors(AuthorRepository repository) {
+        author1 = repository.save(new Author("1", "Author_1")).block();
+        author2 = repository.save(new Author("2", "Author_2")).block();
+        author3 = repository.save(new Author("3", "Author_3")).block();
     }
 
     @ChangeSet(order = "003", id = "insertGenres", author = AUTHOR_NAME, runAlways = true)
-    public void insertGenres(MongockTemplate repository) {
-        genre1 = repository.save(new Genre("1", "Genre_1"));
-        genre2 = repository.save(new Genre("2", "Genre_2"));
-        genre3 = repository.save(new Genre("3", "Genre_3"));
-        genre4 = repository.save(new Genre("4", "Genre_4"));
-        genre5 = repository.save(new Genre("5", "Genre_5"));
-        genre6 = repository.save(new Genre("6", "Genre_6"));
+    public void insertGenres(GenreRepository repository) {
+        genre1 = repository.save(new Genre("1", "Genre_1")).block();
+        genre2 = repository.save(new Genre("2", "Genre_2")).block();
+        genre3 = repository.save(new Genre("3", "Genre_3")).block();
+        genre4 = repository.save(new Genre("4", "Genre_4")).block();
+        genre5 = repository.save(new Genre("5", "Genre_5")).block();
+        genre6 = repository.save(new Genre("6", "Genre_6")).block();
     }
 
     @ChangeSet(order = "004", id = "insertBooks", author = AUTHOR_NAME, runAlways = true)
-    public void insertBooks(MongockTemplate repository) {
+    public void insertBooks(BookRepository repository) {
         book1 = new Book("1", "BookTitle_1", author1, genre1, genre2);
-        repository.save(book1);
+        repository.save(book1).block();
         book2 = new Book("2", "BookTitle_2", author2, genre3, genre4);
-        repository.save(book2);
+        repository.save(book2).block();
         val book3 = new Book("3", "BookTitle_3", author3, genre5, genre6);
-        repository.save(book3);
+        repository.save(book3).block();
     }
 
     @ChangeSet(order = "005", id = "insertComments", author = AUTHOR_NAME, runAlways = true)
-    public void insertComments(MongockTemplate repository) {
+    public void insertComments(CommentRepository repository) {
         val comment1 = new Comment("1", "Book1_Comment_1", book1);
-        repository.save(comment1);
+        repository.save(comment1).block();
         val comment2 = new Comment("2", "Book1_Comment_2", book1);
-        repository.save(comment2);
+        repository.save(comment2).block();
         val comment3 = new Comment("3", "Book2_Comment_3", book2);
-        repository.save(comment3);
+        repository.save(comment3).block();
     }
+
 }
