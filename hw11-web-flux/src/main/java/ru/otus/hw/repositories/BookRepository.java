ce569977fff24a3d3de6a7b0c95eea462db11fae
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2025-01-27 12:34:21 +0300

    feat: Добавить методы в репозиторий книг

--- hw11-web-flux/src/main/java/ru/otus/hw/repositories/BookRepository.java
+++ hw11-web-flux/src/main/java/ru/otus/hw/repositories/BookRepository.java
@@ -1,7 +1,12 @@
 package ru.otus.hw.repositories;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 import ru.otus.hw.models.Book;
 
+import java.util.List;
+
 public interface BookRepository extends ReactiveMongoRepository<Book, String> {
+    Flux<Void> deleteAllBooksByAuthorId(String authorId);
+    Flux<Book> findAllBooksByGenresIdIn(List<String> genreIds);
 }
